cde577e042679c4b642c166d2c380a1299d5e862
SKim041 <sehee1806@gmail.com>
2025-09-04 17:21:24 +0900

    feat(Module, System): 커스텀 Log 카테고리 추가 및 모듈 초기화 코드 작성
    로깅과 모듈 초기화 기능 확장을 위해 커스텀 코드가 추가됨.
    
    - LogLS라는 커스텀 로그 카테고리를 정의하고 선언(LogMacros.h 사용).
    - FLyraStudyModule 클래스를 생성하여 StartupModule과 ShutdownModule 메서드 오버라이드.
    - 기존 기본 모듈 초기화 코드를 커스텀 모듈로 교체.

--- Source/LyraStudy/LyraStudy.cpp
+++ Source/LyraStudy/LyraStudy.cpp
@@ -3,4 +3,21 @@
 #include "LyraStudy.h"
 #include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LyraStudy, "LyraStudy" );
+class FLyraStudyModule : public FDefaultGameModuleImpl
+{
+public:
+	virtual void StartupModule() override;
+	virtual void ShutdownModule() override;
+};
+
+void FLyraStudyModule::StartupModule()
+{
+	FDefaultGameModuleImpl::StartupModule();
+}
+
+void FLyraStudyModule::ShutdownModule()
+{
+	FDefaultGameModuleImpl::ShutdownModule();
+}
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FLyraStudyModule, LyraStudy, "LyraStudy");
